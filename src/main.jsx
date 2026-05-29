88662120bf84129484548d7dfe090c180e77c581
tranquileasewellness <tranquileasewellness@gmail.com>
2026-05-29 20:19:54 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,28 @@
+```jsx id="h3v0qz"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+function App() {
+  return (
+    <div
+      style={{
+        background: "black",
+        color: "white",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+      }}
+    >
+      TranquilEase Wellness
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+```
